0921276724f29a464559f091c1893128b5114a50
luoyuxiang61 <luoyuxiangqop@gmail.com>
2017-11-30 17:44:37 +0800

    登录的动态效果待加入 登录html中有注释掉的i

---
+++ js/login.js
@@ -0,0 +1,50 @@
+// var working = false;
+// $('.login').on('submit', function(e) {
+//   e.preventDefault();
+
+
+//   if (working) return;
+//   working = true;
+//   var $this = $(this),
+//     $state = $this.find('button > .state');
+//   $this.addClass('loading');
+//   $state.html('验证中');
+//   setTimeout(function() {
+//     $this.addClass('ok');
+//     $state.html('登录成功!');
+    
+//     setTimeout(function() {
+//       $state.html('进入主页');
+//       $this.removeClass('ok loading');
+//       working = false;
+//       window.location.href="./index.html";
+//     }, 1000);
+//   }, 2000);
+// });
+
+
+
+$.ajaxSetup (
+  {
+     async: false
+  });
+
+
+
+function checkUser(){
+  var userName = document.getElementById('userName').value;
+  var password = document.getElementById('password').value;
+  var user;
+  console.log(userName);
+  console.log(password);
+  $.post("http://localhost:3000/test?userName="+userName+"&password="+password,function(data){  
+    user = data;
+  })
+
+  if(user==='test') {
+    alert('用户名或密码错误！');
+    return false;
+  }
+  
+  return user!=null;
+}
