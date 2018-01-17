55e8a911ee02d3f73b8b435d803e11a0fe9b48c2
luoyuxiang61 <luoyuxiangqop@gmail.com>
2018-01-17 16:21:18 +0800

    chonggou

---
+++ config.js
@@ -0,0 +1,13 @@
+//后台服务器地址
+var serverIP = "192.168.0.163"
+var serverPort = "3000"
+
+
+//arcgis server服务器地址
+var gisServerIP = "192.168.0.163"
+var gisServerPort = "6080"
+
+
+// jsapi服务器地址（不可在此修改，须修改index.html最下src的地址)
+var jsApiServerIP = "192.168.0.163"
+var jsApiServerPort = "8007"
--- js/config.js
+++
@@ -1,5 +0,0 @@
-var serverIP = "192.168.0.163"
-var serverPort = "3000"
-
-var gisServerIP = "192.168.0.163"
-var gisServerPort = "6080"
