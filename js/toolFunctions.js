f5428b72c63623235fead304c4723d187c88dc8d
luoyuxiang61 <luoyuxiangqop@gmail.com>
2018-01-22 14:42:39 +0800

    next

--- js/toolFunctions.js
+++ js/toolFunctions.js
@@ -1,4 +1,23 @@
-console.log('ssssssssssssssssssssssssssssssss');
-$("#toolContainer>div").click(function () {
-    $("#toolGround").toggle();
+$("#toolContainer>div").click(function (e) {
+    var toolGround = $("#toolGround");
+    var clickedTool = $(e.currentTarget);
+    var isShow = clickedTool.attr('show') == '1' ? true : false;
+    var em = $(clickedTool.children()[2]);
+
+    var ems = $($("#toolContainer em"))
+    var tools = $($("#toolContainer>div"))
+
+
+    if (!isShow) {
+        tools.attr('show', '0')
+        clickedTool.attr('show', '1')
+        ems.css('background-position', '-13px -17px');
+        em.css('background-position', '-12px -177px');
+        toolGround.show();
+    } else {
+        tools.attr('show', '0')
+        ems.css('background-position', '-13px -17px');
+        toolGround.hide()
+    }
+
 })
