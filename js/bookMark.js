eae2b34c2aff0742ed4faa9df800105139e716f9
luoyuxiang61 <luoyuxiangqop@gmail.com>
2018-01-29 11:29:07 +0800

    star

--- js/bookMark.js
+++ js/bookMark.js
@@ -14,7 +14,7 @@ function getBookmarks() {
         success: function (res) {
             nowBookmarks = res;
             for (var i = 0; i < res.length; i++) {
-                $("#starToolDiv").append("<div class='list-group-item' index='" + i + "' onclick='gotoStar(this)' >" + res[i].name + "书签" + "</div>")
+                $("#starToolDiv").append("<div class='list-group-item'>" + "<div class='star-container'>" + "<span index='"+ i +"' onclick='gotoStar(this)'>" + res[i].name + "书签"  +"</span>"  + "<div class='edit'><input type='text'><button type='button' class='btn btn-default btn-sm'><span class='glyphicon glyphicon-ok'></span></button></div>"+ "<button title='取消收藏' type='button' class='btn btn-default btn-sm'><span class=\"glyphicon glyphicon-remove\" aria-hidden=\"true\"></span></button>" + "<button type='button' title='修改名称' class='btn btn-default btn-sm'><span class=\"glyphicon glyphicon-pencil\" aria-hidden=\"true\"></span></button>" +"</div></div>")
             }
         }
     })
