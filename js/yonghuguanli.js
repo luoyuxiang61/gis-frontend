83d23ced0f54ff99be79b3c41b313a752046a7fd
luoyuxiang61 <luoyuxiangqop@gmail.com>
2018-02-24 16:52:13 +0800

    rest

--- js/yonghuguanli.js
+++ js/yonghuguanli.js
@@ -1,4 +1,5 @@
 var departments = []
+var layers = null
 
 $.ajax({
     type: 'get',
@@ -47,9 +48,40 @@ $.ajax({
             $("#grpContainer").append("<button title='配置权限组' class='btn btn-primary btn-sm grpCfg'><i class='fa fa-cog'></i></button> <button title='删除权限组' class='btn btn-default btn-sm grpEdit'><i class='fa fa-trash-alt'></i></button><button title='添加权限组' class='btn btn-default btn-sm grpEdit'><i class='fa fa-plus'></i></button>")
             getUsersInDepa(depaId)
 
+            //添加权限组
             $("[title=添加权限组]").click(function () {
                 var grayBack = $("#grayBack")
                 var addGrpDiv = $("#addGrpDiv")
+
+                var refreshLayers = function (layersForTree) {
+                    layers = layersForTree;
+                    var sonshtml = '';
+                    for (var i = 0; i < layersForTree.length; i++) {
+                        sonshtml += "<div class='list-group-item father'><input type='checkbox' >" + "<strong>" + layersForTree[i].father.DisplayName + "</strong></div>"
+                        sonshtml += "<ul class='list-group son' style='padding-left:15px;margin-bottom:3px'>"
+                        for (var j = 0; j < layersForTree[i].sons.length; j++) {
+                            sonshtml += "<li layerType='" + layersForTree[i].sons[j].LayerType + "' class='list-group-item son' layerId='" + layersForTree[i].sons[j].id + "'>" + "<input type='checkbox'>" + layersForTree[i].sons[j].DisplayName + "</li>"
+                        }
+                        sonshtml += "</ul>"
+                    }
+
+                    $("#layerList").empty()
+                    $("#layerList").append(
+                        "<div class='list-group' onselectstart='return false'> " + sonshtml + "</div>"
+                    )
+                }
+
+                if (!layers) {
+                    $.ajax({
+                        type: 'post',
+                        url: "http://" + serverIP + ":" + serverPort + "/layersForTree",
+                        success: refreshLayers
+                    })
+                } else {
+                    refreshLayers(layers)
+                }
+
+
                 grayBack.show()
                 addGrpDiv.show()
                 $("#closeEdit").click(function () {
@@ -78,7 +110,6 @@ $.ajax({
                             grpId: grpId
                         },
                         success: function (users) {
-                            console.log(users)
                             for (var k = 0; k < users.length; k++) {
                                 var user = users[k]
                                 $("#mainT").append("<tr><td class='col-xs-2'>" + "<span style='line-height:30px;display:inline-block;height:30px;width:auto;'>" + user.UserName + "</span></td><td class='col-xs-2'>" + "<span style='line-height:30px;display:inline-block;height:30px;width:auto;'>" + user.Password + "</span></td><td class='col-xs-4'><span style='line-height:30px;display:inline-block;height:30px;width:auto;'>sss</span></td><td class='col-xs-2'><span style='line-height:30px;display:inline-block;height:30px;width:auto;'>sss</span></td><td class='col-xs-2'><button class='btn btn-default btn-sm userEdit'><i class='fas fa-edit'></i></button><button class='btn btn-default btn-sm userEdit'><i class='fas fa-times'></i></button></td></tr>")
