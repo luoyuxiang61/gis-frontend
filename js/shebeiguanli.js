c0c4b0c41c03a187feb45d8938214e4d1c43b730
luoyuxiang61 <luoyuxiangqop@gmail.com>
2018-03-08 15:32:27 +0800

    shebeiguanli demo

---
+++ js/shebeiguanli.js
@@ -0,0 +1,8 @@
+$("#deviceType li").click(function (e) {
+    $("#deviceType li").removeClass('active')
+    e.currentTarget.classList.add('active')
+})
+
+$("#syncDevices").click(function () {
+    console.log('shuaxin')
+})
