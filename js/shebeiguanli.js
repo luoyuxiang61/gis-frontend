2009d1115c4ff5d114d915422edb798b3544235c
luoyuxiang61 <luoyuxiangqop@gmail.com>
2018-03-12 17:09:18 +0800

    edit device to do more

--- js/shebeiguanli.js
+++ js/shebeiguanli.js
@@ -1,8 +1,5 @@
 function getDevices() {
-
-    console.log(arguments)
     var args = Array.prototype.slice.call(arguments)
-    console.log(args)
     $.ajax({
         url: "http://" + serverIP + ":" + serverPort + "/allDevices",
         type: 'post',
@@ -11,19 +8,20 @@ function getDevices() {
             type: args[1]
         },
         success: function (devices) {
-            console.log(devices)
             var mt = $("#mainT")
             mt.empty()
             for (var i = 0; i < devices.length; i++) {
                 var de = devices[i]
-                var users = de.Users.map(function (x) { return x.UserName }).join(",")
+                var users = de.Users.map(function (x) { return x.UserName }).join("  ,  ")
                 mt.append("<tr><td class='col-xs-3'>" + de.uuid + "</td><td class='col-xs-2'>" + de.model + (de.deviceType === 0 ? '(手机)' : '(平板)') + "</td><td class='col-xs-2'>" + (de.os === 0 ? "android " : "ios ") + de.osVersion + "</td>\
-                <td class='col-xs-2'>"+ users + "</td><td class='col-xs-1'><i class='fas fa-check'></i></td><td class='col-xs-1'><i class='fas fa-check'></i></td><td class='col-xs-1'><i class='fas fa-edit fa-1x editDevice'></i></td></tr>")
+                <td class='col-xs-2'>"+ users + "</td><td class='col-xs-1'><i class='" + (de.online === 1 ? "fas fa-check" : "") + "'></i></td><td class='col-xs-1'><i class='" + (de.offline === 1 ? "fas fa-check" : "") + "'></i></td><td class='col-xs-1'><i class='fas fa-edit fa-1x editDeviceBtn'></i></td></tr>")
             }
 
-            $("i.editDevice").click(function () {
-                console.log('asdadasd')
+            $("i.editDeviceBtn").click(function () {
+                $("#editDeviceDiv").css('display', 'block')
+                $("#grayBack").show()
             })
+
         }
     })
 }
@@ -54,4 +52,9 @@ $("#deviceType li").click(function (e) {
 
 $("#syncDevices").click(function () {
     console.log('shuaxin')
+})
+
+$("#closeEdit").click(function () {
+    $(".top").hide()
+    $("#grayBack").hide()
 })
